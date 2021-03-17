58690e16dadb95e33b5e2cc6265048e389aca978
김성권 <gimseong-gwon@gimseong-gwon-ui-MacBookPro.local>
2021-03-17 21:45:44 +0900

    chapter5 mongoDB 접합

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 // This is the main entry point of our application
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
+require('dotenv').config();
+const db = require('./db');
+const models = require('./models');
 
 // .env 파일에 명시된 포트 또는 포트 4000에서 서버를 실행
 const port = process.env.PORT || 4000;
+// DB_HOST 값을 변수로 저장
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
   {id: '1', content: 'This is a note', author: 'Adam Scott'},
@@ -35,25 +40,27 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'hello World!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     }
   },
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote:  async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: 'Adam Scott'
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     }
   }
 };
 
 const app = express();
+
+// DB에 연결
+db.connect(DB_HOST);
 
 // 아폴로 서버 설정
 const server = new ApolloServer({typeDefs, resolvers});
